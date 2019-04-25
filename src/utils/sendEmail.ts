8b90db09ee4162a14b3f2ed77260b403fa3b4c73
Daniel Stefan Klose <daniel-stefan.klose@udo.edu>
2019-04-25 21:18:13 +0200

    added sending only on production

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -4,24 +4,26 @@ import * as SparkPost from 'sparkpost'
 const client = new SparkPost()
 
 export const sendEmail = async (recipient: string, link: string) => {
-  const res = await client.transmissions.send({
-    options: {
-      sandbox: true
-    },
-    content: {
-      from: 'testing@sparkpostbox.com',
-      subject: 'Confirm email',
-      html: `<html>
-        <body>
-          <h1>please confirm your email</h1>
-          <a href="${link}">click here</a>
-        </body>
-      </html>`
-    },
-    recipients: [
-      { address: recipient }
-    ],
-  })
-
-  console.log(res)
+  if (process.env.NODE_ENV === 'production') {
+    await client.transmissions.send({
+      options: {
+        sandbox: true
+      },
+      content: {
+        from: 'testing@sparkpostbox.com',
+        subject: 'Confirm email',
+        html: `<html>
+          <body>
+            <h1>please confirm your email</h1>
+            <a href="${link}">click here</a>
+          </body>
+        </html>`
+      },
+      recipients: [
+        { address: recipient }
+      ],
+    })
+  } else {
+    console.log({ recipient, link })
+  }
 }
